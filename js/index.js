2c1e152d60e50e5ade496244ad32c56dad2fdee3
atibioke <eudoxusganium@gmail.com>
2022-02-08 13:20:28 +0100

    working on the responsiveness of the hero section

--- js/index.js
+++ js/index.js
@@ -1,37 +1,44 @@
 
 // QUADRATIC EQUATION
 
+// let a = 1;
+// let b = 3;
+// let c = 2;
+
+// const num_1 = Math.pow(b, 2);
+// const num_2 = 4*a*c;
+// const num_3 = (num_1 - num_2);
+// const denominator = (2*a);
+// const D = Math.sqrt(num_3);
+// const num_4 = (-b+D);
+// const num_5 = (-b-D);
+// const result_1 = (num_4/denominator);
+// const result_2 = (num_5/denominator);
+// console.log(result_1)
+// console.log(result_2);
+
+
+HERON'S FORMULA
+
 let a = 3;
-let b = 3;
-let c = 2;
+let b = 4;
+let c = 5;
 
-const num_1 = Math.pow(b, 2);
-const num_2 = 4*a*c;
-const num_3 = (num_1 - num_2);
-const denominator = (2*a);
-const D = Math.sqrt(num_3);
-const num_4 = (-b+D);
-const num_5 = (-b-D);
-const result_1 = (num_4/denominator);
-const result_2 = (num_5/denominator);
-console.log(result_1)
-console.log(result_2);
-
-
-// HERON'S FORMULA
-
-// let a = 3;
-// let b = 4;
-// let c = 5;
-
-// const S = ((a + b + c) / 2);
+const S = ((a + b + c) / 2);
 // const num_1 = (S - a);
 // const num_2 = (S - b);
 // const num_3 = (S - c);
-// const D = (S * num_1*num_2*num_3);
-// const Area = Math.sqrt(D);
+const D = (S * num_1*num_2*num_3);
+const Area = Math.sqrt(D);
 
-// console.log(Area);
+console.log(Area);
+
+
+// let a = 1;
+// let b = 3;
+// let c = 2;
+
+
 
 
 
