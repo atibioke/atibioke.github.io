74488836609a21dff884000aed08b29a96fe6c93
atibioke <eudoxusganium@gmail.com>
2022-02-19 09:00:15 +0100

    algorithm solution

--- algorithm_solutions/index.js
+++ algorithm_solutions/index.js
@@ -3,32 +3,45 @@
 
 
 // function findMultipleOfTwoNumbers(a, b, range) {
-//     n_1 = Math.floor(range/a)
-//     n_2 = Math.floor(range/b)
-//     n_3 = Math.floor(range/(a*b))
-//      sum_1 = ((n_1)*[2*a + ((n_1)-1)*a])/2
-//     sum_2 = ((n_2)*[2*b + ((n_2)-1)*b])/2
-//     sum_3 = ((n_3)*[2*a*b + ((n_3)-1)*a*b])/2
+//   let  n_1 = Math.floor(range/a)
+//   let n_2 = Math.floor(range/b)
+//    let n_3 = Math.floor(range/(a*b))
+//    let  sum_1 = ((n_1)*[2*a + ((n_1)-1)*a])/2
+  
+//   let  sum_2 = ((n_2)*[2*b + ((n_2)-1)*b])/2
+ 
+//   let  sum_3 = ((n_3)*[2*a*b + ((n_3)-1)*a*b])/2
+
 //     return((sum_1 + sum_2) - sum_3)
 //   }
+//   findMultipleOfTwoNumbers(a, b, range)
 
 
 // END
 
 // SUM FIBONACCI EVEN TERMS
 
-
-function evenSumOfFibonacci(n){
-
-    for(let n=3; n<=33; n+=3){
-  let num = Math.floor(( Math.pow((1+(Math.pow(5, 0.5)))/2, n)  - Math.pow((1-(Math.pow(5, 0.5)))/2, n))*(1/(Math.pow(5, 0.5))));
-    }
-     let sum = num+=num
-  
-   console.log(sum)
+function Fib(n) {
+    let num = Math.floor(
+      (Math.pow((1 + Math.pow(5, 0.5)) / 2, n) -
+        Math.pow((1 - Math.pow(5, 0.5)) / 2, n)) *
+        (1 / Math.pow(5, 0.5))
+    );
+    return num;
   }
-  evenSumOfFibonacci(n)
-   
+  function evenSumOfFibonacci(m) {
+    let sum = 0;
+    let i = 3;
+    while (Fib(i) <= m) {
+      sum += Fib(i);
+      i += 3;
+    }
+    console.log(sum);
+    return sum
+  }
+  evenSumOfFibonacci(1000000000);
+  console.log(Fib(34));
+
     
     
 
