30abbf2dd229f70db66b085a3e586784c7eda92b
atibioke <eudoxusganium@gmail.com>
2022-02-28 00:05:57 +0100

    working on my country flag project

--- country-flag/country.js
+++ country-flag/country.js
@@ -1,13 +1,60 @@
 const countryButtonElement = document.querySelector("button")
+
 countryButtonElement.addEventListener("click", function(amebo){
     
-    const randomIndex = Math.floor(Math.random() * countryWithImages.length ) 
+    const randomIndex = Math.floor(Math.random() * countryWithImages.length )
     const selectedCountry = countryWithImages[randomIndex]
     
     document.querySelector("img")
     .setAttribute("src", `https:${selectedCountry.file_url}`)
     
-    const allPElement =  document.querySelectorAll("p")
-    allPElement[0].textContent = selectedCountry.name
-    allPElement[1].textContent = selectedCountry.alpha3
+    const optionIndex = [randomIndex];
+
+    for(let i = 1; i <= 3; i++){
+        let randomOptionIndex = Math.floor(Math.random() * countryWithImages.length)
+        optionIndex.push(randomOptionIndex)
+    }
+
+    const shuffledOptionIndex = optionIndex.sort((a, b)=> a-b)
+
+    var container = document.getElementById('container');
+
+    container.innerHTML = ""
+    for(let i = 0; i < shuffledOptionIndex.length; i++){
+        const selectedCountry = countryWithImages[optionIndex[i]]
+
+        var checkbox = document.createElement('input');
+        checkbox.type = 'radio';
+        checkbox.class = 'checkbox';
+        checkbox.name = 'country';
+        checkbox.value = selectedCountry.name;
+      
+        var label = document.createElement('label')
+        label.htmlFor = selectedCountry.name;
+        label.appendChild(document.createTextNode(selectedCountry.name));
+        
+        var br = document.createElement('br');
+      
+        container.appendChild(checkbox);
+        container.appendChild(label);
+        container.appendChild(br);
+    }
+
+    
+    const countryForm = document.querySelector('#container');
+
+
+    countryForm.addEventListener("click", function(e) {
+        if (e.target.tagName.toUpperCase() === "INPUT") {
+            var value = e.target.value.toLowerCase(); 
+
+           if(selectedCountry.name.toLowerCase() === value){
+               alert("Correct")
+           } else {
+               alert("Incorrect")
+           }
+        }
+    });
+
+
 })
