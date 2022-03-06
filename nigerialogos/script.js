a85ab71388641fea2d7f4d372deff9bd5d061932
atibioke <eudoxusganium@gmail.com>
2022-03-06 17:48:26 +0100

    working on nigerialogos

--- nigerialogos/script.js
+++ nigerialogos/script.js
@@ -1,3 +1,40 @@
-const image = document.querySelector('card-image');
-const title = document.querySelector('title');
-const category = document.querySelector('category');
+const logoContainer = document.getElementById('logoContainer');
+
+
+
+
+for(let i = 0; i < companyLogos.length; i++) {
+    const fileName = companyLogos[i].filename;
+    const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
+    logoContainer.innerHTML += `
+    <div class="card">
+        <img src="${logoUrl}" alt="${fileName}" class="card-image">
+    </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
