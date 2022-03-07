67df81bc72fe9bb147715f05e0e7e9e23be7de77
atibioke <eudoxusganium@gmail.com>
2022-03-07 13:37:59 +0100

    working on nigerialogos

--- nigerialogos/script.js
+++ nigerialogos/script.js
@@ -1,11 +1,49 @@
 const logoContainer = document.getElementById("logoContainer");
+const allAnchors=document.getElementsByTagName('a');
 
 
 for (let i = 0; i < companyLogos.length; i++) {
-  const fileName = companyLogos[i].filename;
-  const title = companyLogos[i].title;
-  const category = companyLogos[i].category.filter((a) => a.length).join(' / ');
-  const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
+pickOutCompany(companyLogos[i])
+}
+
+document.getElementById("Categories").addEventListener('change', function(e) {
+    console.log(this.value, e.target.value);
+    let categoryValue=this.value;
+    logoContainer.innerHTML="";
+
+    for(let i=0;i < companyLogos.length; i++ ){
+       if(companyLogos[i].category.includes(categoryValue)){
+        pickOutCompany(companyLogos[i])
+
+       }
+    }
+
+  });
+
+
+//Getting logos that start with a letter
+
+for(let i = 0; i < allAnchors.length; i++){
+    allAnchors[i].addEventListener('click',function(e){
+        const presentLetter=e.currentTarget.textContent;
+        logoContainer.innerHTML="";
+
+        for(let i=0;i < companyLogos.length; i++ ){
+            if(companyLogos[i].title.startsWith(presentLetter)){
+                pickOutCompany(companyLogos[i])
+
+            }
+       }
+
+
+    })
+}
+
+function pickOutCompany(companyEntity){
+    const fileName = companyEntity.filename;
+    const title = companyEntity.title;
+    const category = companyEntity.category.filter((a) => a.length).join(' / ');
+    const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
     logoContainer.innerHTML += `
       <div class="card-box">
           <div class="card">
@@ -19,38 +57,7 @@ for (let i = 0; i < companyLogos.length; i++) {
            </h3>
       </div>
       `;
+
+
 }
-
-document.getElementById("Categories").addEventListener('change', function(e) {
-    console.log(this.value, e.target.value);
-    let categoryValue=this.value;
-    logoContainer.innerHTML="";
-
-    for(let i=0;i < companyLogos.length; i++ ){
-       if(companyLogos[i].category.includes(categoryValue)){
- const fileName = companyLogos[i].filename;
-  const title = companyLogos[i].title;
-  const category = companyLogos[i].category.filter((a) => a.length).join(' / ');
-  const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
-  logoContainer.innerHTML += `
-    <div class="card-box">
-        <div class="card">
-        <img src="${logoUrl}" alt="${fileName}" class="card-image">
-    </div>
-        <p class="title">
-            <a href="" >${title}</a>
-        </p>
-        <h3>
-        <span class="category">${category}</span>
-         </h3>
-    </div>
-    `;
-
-       }
-    }
-
-  });
-
-
-
 
