88311358a61a0191d6858bb3d92bb9814866bede
atibioke <eudoxusganium@gmail.com>
2022-03-07 14:25:44 +0100

    working on nigerialogos

--- nigerialogos/script.js
+++ nigerialogos/script.js
@@ -1,63 +1,53 @@
 const logoContainer = document.getElementById("logoContainer");
-const allAnchors=document.getElementsByTagName('a');
+const allAnchors = document.getElementsByTagName("a");
 
-
-for (let i = 0; i < companyLogos.length; i++) {
-pickOutCompany(companyLogos[i])
+function pickOutCompany(companyEntity) {
+  const fileName = companyEntity.filename;
+  const title = companyEntity.title;
+  const category = companyEntity.category.filter((a) => a.length).join(" / ");
+  const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
+  logoContainer.innerHTML += `
+        <div class="card-box">
+            <div class="card">
+                <img src="${logoUrl}" alt="${fileName}" class="card-image">
+            </div>
+            <p class="title">
+                <a href="" >${title}</a>
+            </p>
+            <h3>
+            <span class="category">${category}</span>
+             </h3>
+        </div>
+        `;
 }
 
-document.getElementById("Categories").addEventListener('change', function(e) {
-    console.log(this.value, e.target.value);
-    let categoryValue=this.value;
-    logoContainer.innerHTML="";
+for (let i = 0; i < companyLogos.length; i++) {
+  pickOutCompany(companyLogos[i]);
+}
 
-    for(let i=0;i < companyLogos.length; i++ ){
-       if(companyLogos[i].category.includes(categoryValue)){
-        pickOutCompany(companyLogos[i])
+document.getElementById("Categories").addEventListener("change", function (e) {
+  console.log(this.value, e.target.value);
+  let categoryValue = this.value;
+  logoContainer.innerHTML = "";
 
-       }
+  for (let i = 0; i < companyLogos.length; i++) {
+    if (companyLogos[i].category.includes(categoryValue)) {
+      pickOutCompany(companyLogos[i]);
     }
-
-  });
-
+  }
+});
 
 //Getting logos that start with a letter
 
-for(let i = 0; i < allAnchors.length; i++){
-    allAnchors[i].addEventListener('click',function(e){
-        const presentLetter=e.currentTarget.textContent;
-        logoContainer.innerHTML="";
+for (let i = 0; i < allAnchors.length; i++) {
+  allAnchors[i].addEventListener("click", function (e) {
+    const presentLetter = e.currentTarget.textContent;
+    logoContainer.innerHTML = "";
 
-        for(let i=0;i < companyLogos.length; i++ ){
-            if(companyLogos[i].title.startsWith(presentLetter)){
-                pickOutCompany(companyLogos[i])
-
-            }
-       }
-
-
-    })
+    for (let i = 0; i < companyLogos.length; i++) {
+      if (companyLogos[i].title.startsWith(presentLetter)) {
+        pickOutCompany(companyLogos[i]);
+      }
+    }
+  });
 }
-
-function pickOutCompany(companyEntity){
-    const fileName = companyEntity.filename;
-    const title = companyEntity.title;
-    const category = companyEntity.category.filter((a) => a.length).join(' / ');
-    const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
-    logoContainer.innerHTML += `
-      <div class="card-box">
-          <div class="card">
-          <img src="${logoUrl}" alt="${fileName}" class="card-image">
-      </div>
-          <p class="title">
-              <a href="" >${title}</a>
-          </p>
-          <h3>
-          <span class="category">${category}</span>
-           </h3>
-      </div>
-      `;
-
-
-}
-
