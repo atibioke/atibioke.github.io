2d868bfcc9fa52044d41c4af49cca2757e3f606e
atibioke <eudoxusganium@gmail.com>
2022-03-07 12:08:31 +0100

    working on nigerialogos

--- nigerialogos/script.js
+++ nigerialogos/script.js
@@ -1,7 +1,4 @@
 const logoContainer = document.getElementById("logoContainer");
-const allOptions= document.querySelectorAll('option');
-
-const categoriesContainer = document.querySelector(".yimu");
 
 
 for (let i = 0; i < companyLogos.length; i++) {
@@ -9,23 +6,20 @@ for (let i = 0; i < companyLogos.length; i++) {
   const title = companyLogos[i].title;
   const category = companyLogos[i].category.filter((a) => a.length).join(' / ');
   const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
-  logoContainer.innerHTML += `
-    <div class="card-box">
-        <div class="card">
-        <img src="${logoUrl}" alt="${fileName}" class="card-image">
-    </div>
-        <p class="title">
-            <a href="" >${title}</a>
-        </p>
-        <h3>
-        <span class="category">${category}</span>
-         </h3>
-    </div>
-    `;
+    logoContainer.innerHTML += `
+      <div class="card-box">
+          <div class="card">
+          <img src="${logoUrl}" alt="${fileName}" class="card-image">
+      </div>
+          <p class="title">
+              <a href="" >${title}</a>
+          </p>
+          <h3>
+          <span class="category">${category}</span>
+           </h3>
+      </div>
+      `;
 }
-
-
-
 
 document.getElementById("Categories").addEventListener('change', function(e) {
     console.log(this.value, e.target.value);
@@ -33,13 +27,8 @@ document.getElementById("Categories").addEventListener('change', function(e) {
     logoContainer.innerHTML="";
 
     for(let i=0;i < companyLogos.length; i++ ){
-        
-       // console.log(companyLogos[i].category)
        if(companyLogos[i].category.includes(categoryValue)){
-        
-        console.log(companyLogos[i])
-
-        const fileName = companyLogos[i].filename;
+ const fileName = companyLogos[i].filename;
   const title = companyLogos[i].title;
   const category = companyLogos[i].category.filter((a) => a.length).join(' / ');
   const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
