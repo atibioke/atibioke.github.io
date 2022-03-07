1b5dff24643b7be334e9cef333c0cb213bf68e63
atibioke <eudoxusganium@gmail.com>
2022-03-07 09:17:21 +0100

    working on nigerialogos

--- nigerialogos/script.js
+++ nigerialogos/script.js
@@ -3,7 +3,7 @@ const logoContainer = document.getElementById("logoContainer");
 for (let i = 0; i < companyLogos.length; i++) {
   const fileName = companyLogos[i].filename;
   const title = companyLogos[i].title;
-  const category = companyLogos[i].category.join(' / ');
+  const category = companyLogos[i].category.filter((a) => a.length).join(' / ');
   const logoUrl = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
   logoContainer.innerHTML += `
     <div class="card-box">
@@ -11,7 +11,7 @@ for (let i = 0; i < companyLogos.length; i++) {
         <img src="${logoUrl}" alt="${fileName}" class="card-image">
     </div>
         <p>
-            <a href="">${title}</a>
+            <a href="" class="title">${title}</a>
 
         </p>
         <h3>
