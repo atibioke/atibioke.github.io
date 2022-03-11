d255425642dd47fe1923c79af246ed87b038ec87
atibioke <eudoxusganium@gmail.com>
2022-03-11 19:38:40 +0100

    working on country flag

--- country_slider/main.js
+++ country_slider/main.js
@@ -1,1 +1,13 @@
 const container = document.querySelector('.container')
+
+
+function countryImagesSlider(){
+
+const randomIndex = Math.floor(Math.random() * countryWithImages.length )
+   
+const selectedCountry = countryWithImages[randomIndex]
+
+document.querySelector("img")
+.setAttribute("src", `https:${selectedCountry.file_url}`)
+
+}
