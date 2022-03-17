2b729215756673aa51698ff921d568e7340e6f12
atibioke <eudoxusganium@gmail.com>
2022-03-17 17:29:37 +0100

    working on flag image slider

--- country_slider/main.js
+++ country_slider/main.js
@@ -1,18 +1,91 @@
-const container = document.querySelector('.container')
-let progress = document.getElementsByClassName("progress-bar")[0];
-console.log(progress)
-
-
- setInterval(() => {
-    const randomIndex = Math.floor(Math.random() * countryWithImages.length )
-   
-    const selectedCountry = countryWithImages[randomIndex]
+function flagQuiz(amebo){
     
-    document.querySelector("img")
-    .setAttribute("src", `https:${selectedCountry.file_url}`)
+  const randomIndex = Math.floor(Math.random() * countryWithImages.length )
+ 
+  const selectedCountry = countryWithImages[randomIndex]
+  
+  document.querySelector("img")
+  .setAttribute("src", `https:${selectedCountry.file_url}`)
+  
+  const optionIndex = [randomIndex];
+
+  for(let i = 1; i <= 3; i++){
+      let randomOptionIndex = Math.floor(Math.random() * countryWithImages.length)
+      optionIndex.push(randomOptionIndex)
+  }
 
 
-}, 2500);
+  const shuffledOptionIndex = optionIndex.sort(() => .5 - Math.random())
+
+
+
+  let container = document.getElementById('container');
+
+  container.innerHTML = "";
+ 
+  for(let i = 0; i < shuffledOptionIndex.length; i++){
+      const selectedCountry = countryWithImages[shuffledOptionIndex[i]]
+
+      let checkbox = document.createElement('input');
+      checkbox.type = 'radio';
+      checkbox.class = 'checkbox';
+      checkbox.name = 'country';
+      checkbox.value = selectedCountry.name;
+    
+      let label = document.createElement('label')
+      label.htmlFor = selectedCountry.name;
+      label.appendChild(document.createTextNode(selectedCountry.name));
+      
+      let br = document.createElement('br');
+    
+      container.appendChild(checkbox);
+      container.appendChild(label);
+      container.appendChild(br);
+  }
+
+  
+  const countryForm = document.querySelector('#container');
+
+
+  countryForm.addEventListener("click", function(e) {
+      if (e.target.tagName.toUpperCase() === "INPUT") {
+          var value = e.target.value.toLowerCase(); 
+       if(selectedCountry.name.toLowerCase() === value){
+            <span></span>
+           } else {
+            <span></span>
+         }
+      }
+  });
+
+
+}
+
+
+setInterval(flagQuiz, 2500)
+
+
+
+
+
+
+
+
+// const container = document.querySelector('.container')
+// let progress = document.getElementsByClassName("progress-bar")[0];
+// console.log(progress)
+
+
+//  setInterval(() => {
+//     const randomIndex = Math.floor(Math.random() * countryWithImages.length )
+   
+//     const selectedCountry = countryWithImages[randomIndex]
+    
+//     document.querySelector("img")
+//     .setAttribute("src", `https:${selectedCountry.file_url}`)
+
+
+// }, 2500);
 
 
 // setInterval(() => {
@@ -25,17 +98,17 @@ console.log(progress)
 
 
 
-function move() {
-        let a = setInterval(function() {
-          for(let i=0; i<=100; i+=1){
-            progress.value =  i;
-          }
-            if (progress.value == 100) {
-              clearInterval(a);
-            }
-          }, );
+// function move() {
+//         let a = setInterval(function() {
+//           for(let i=0; i<=100; i+=1){
+//             progress.value =  i;
+//           }
+//             if (progress.value == 100) {
+//               clearInterval(a);
+//             }
+//           }, );
 
-    }
+//     }
  
 
 
